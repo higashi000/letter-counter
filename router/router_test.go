d3fbc418aee3d48981a484b702c65cf899d3bc11
higashi000 <higashi.ict@gmail.com>
2020-08-17 22:44:16 +0900

    Add letter count test

---
+++ router/router_test.go
@@ -0,0 +1,11 @@
+package router
+
+import "testing"
+
+func TestCountLetter(t *testing.T) {
+	availableSpace, noSpace := CountLetter("こんにちは　 My name is higashi.　")
+
+	if availableSpace != 27 && noSpace != 21 {
+		t.Fatal("failed CountLetter test")
+	}
+}
